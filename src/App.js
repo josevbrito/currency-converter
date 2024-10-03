f3a3ac0dca8ca3ef0946d0e1ab1c4aace4731d36
josevbrito <josevictorbritoc@gmail.com>
2024-10-03 15:41:53 -0300

    feat: :sparkles: Dark Mode in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CurrencyConverter from './components/CurrencyConverter';
 import './App.css';
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  // change between themes
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  }
+
+  // load theme preference saved in local storage
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('darkMode') === 'true';
+    setDarkMode(savedTheme);
+  }, []);
+
+  // save theme preference saved in local storage
+  useEffect(() => {
+    localStorage.setItem('darkMode', darkMode);
+  }, [darkMode]);
+
   return (
-    <div className="App">
-      <CurrencyConverter />
+    <div className={`App ${darkMode ? 'dark-mode' : 'light-mode'}`}>
+      <CurrencyConverter/>
+      <button className='theme-toggle' onClick={toggleTheme}>
+        Switch to {darkMode ? 'Light Mode' : 'Dark Mode'}
+      </button>
     </div>
   );
 };
